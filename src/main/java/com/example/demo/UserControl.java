f2b5c8d59ab480ad3c48759d9f2c899de7507c59
dds <android_shuai@163.com>
2020-02-20 14:14:19 +0800

    添加两个接口

--- src/main/java/com/example/demo/UserControl.java
+++ src/main/java/com/example/demo/UserControl.java
@@ -1,14 +1,35 @@
 package com.example.demo;
 
 
+import com.example.demo.bean.RoomInfo;
+import com.example.demo.bean.UserBean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 public class UserControl {
 
     @RequestMapping("/")
     public String index() {
-        return "Hello World";
+        return "welcome to my webRTC demo";
     }
+
+    @RequestMapping("/roomList")
+    public List<String> roomList() {
+        ConcurrentHashMap<String, RoomInfo> rooms = MemCons.rooms;
+        return new ArrayList<>(rooms.keySet());
+    }
+
+    @RequestMapping("/userList")
+    public List<UserBean> userList() {
+        ConcurrentHashMap<String, UserBean> userBeans = MemCons.userBeans;
+        Collection<UserBean> values = userBeans.values();
+        return new ArrayList<>(values);
+    }
+
 }
--- src/main/java/com/example/demo/bean/UserBean.java
+++ src/main/java/com/example/demo/bean/UserBean.java
@@ -1,47 +1,62 @@
 package com.example.demo.bean;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.websocket.Session;
 
 public class UserBean {
-    private DeviceSession[] sessions = new DeviceSession[2];
+
+    @JsonFormat
     private String userId;
+    @JsonFormat
     private String avatar;
+    @JsonIgnore
+    private DeviceSession[] sessions = new DeviceSession[2];
 
     public UserBean(String userId, String avatar) {
         this.userId = userId;
         this.avatar = avatar;
     }
 
+    @JsonIgnore
     public void setPhoneSession(Session session, int device) {
         this.sessions[0] = new DeviceSession(session, device);
     }
 
+    @JsonIgnore
     public void setPcSession(Session session, int device) {
         this.sessions[1] = new DeviceSession(session, device);
     }
 
+    @JsonIgnore
     public Session getPhoneSession() {
         if (sessions[0] == null) return null;
         return sessions[0].getSession();
     }
 
+    @JsonIgnore
     public Session getPcSession() {
         if (sessions[1] == null) return null;
         return sessions[1].getSession();
     }
 
+    @JsonIgnore
     public String getUserId() {
         return userId;
     }
 
+    @JsonIgnore
     public void setUserId(String userId) {
         this.userId = userId;
     }
 
+    @JsonIgnore
     public String getAvatar() {
         return avatar;
     }
 
+    @JsonIgnore
     public void setAvatar(String avatar) {
         this.avatar = avatar;
     }
@@ -62,11 +77,12 @@ public class UserBean {
         return this.userId.equals(user.getUserId());
     }
 
-
+    @JsonIgnore
     public DeviceSession[] getSessions() {
         return sessions;
     }
 
+    @JsonIgnore
     public void setSessions(DeviceSession[] sessions) {
         this.sessions = sessions;
     }
